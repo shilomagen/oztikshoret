41d8858979503412dd11a5c1468f663c7564ac6a
Shilo Mangam <smangam@gmail.com>
2017-10-31 09:33:08 +0200

    created static server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pdfService = require('./services/pdfcreator/pdf-creator');
+const pdfService = require('./services/pdfcreator/pdfCreator');
+const path = require('path');
 require('dotenv').config();
 const PORT = process.env.PORT || 8080;
 const app = express();
-app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.post('/api/create-offer', async (req, res) => {
   try {
@@ -16,6 +18,10 @@ app.post('/api/create-offer', async (req, res) => {
     res.status(500).end();
   }
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`App is up and running on port ${PORT}`);
 });
